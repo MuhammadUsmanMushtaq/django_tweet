9c64d758ff217b28af9707abd68449b6533d1999
Muhammad Usman <usman820901@gmail.com>
2024-11-21 13:52:59 +0100

    logic-done

--- tweet/tweet/urls.py
+++ tweet/tweet/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
     #always put this path at the end, this will enable hot_relaod tailwind
     path("__reload__/", include("django_browser_reload.urls")),
