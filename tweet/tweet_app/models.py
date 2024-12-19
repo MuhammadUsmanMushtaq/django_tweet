852e5c6dabf005360df5d4b3d56f8ab9635866cf
Muhammad Usman <usman820901@gmail.com>
2024-12-19 18:48:43 +0100

    update-forms

--- tweet/tweet_app/models.py
+++ tweet/tweet_app/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    photo = models.ImageField( upload_to='photos/',blank=True, null=True)
+    photo = models.ImageField(null=True, blank=True, upload_to='photos/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
